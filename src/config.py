a5e234ac4fd6fba11bfc95067991fc4c8cf63e31
nikawamikan <wkwktktkkrkr@outlook.jp>
2023-09-27 11:41:09 +0900

    update: スラッシュコマンド対応
    DISCORD_GUILD_IDSの追加
    ついでにALLOW_PROCESS_KILL_COMMAND消した

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 import os
 
 # Fixed value
-VERSION = '0.1.2'
+VERSION = '0.1.3'
 
 # use environment var in os
 # required
@@ -17,18 +17,14 @@ CONOHA_API_NETWORK_SERVICE = os.environ.get('CONOHA_API_NETWORK_SERVICE')
 CONOHA_API_VM_PLAN_FLAVOR_UUID = os.environ.get('CONOHA_API_VM_PLAN_FLAVOR_UUID')
 
 # option
-VM_AND_IMAGE_NAME = 'asagao-for-minecraft-'+os.environ.get('VM_AND_IMAGE_NAME', '') if os.environ.get('VM_AND_IMAGE_NAME', '') != '' else 'asagao-for-minecraft'
+VM_AND_IMAGE_NAME = 'asagao-for-minecraft-' + \
+    os.environ.get('VM_AND_IMAGE_NAME', '') if os.environ.get('VM_AND_IMAGE_NAME', '') != '' else 'asagao-for-minecraft'
 ADMIN_USER_ID = os.environ.get('ADMIN_USER_ID', '')
 DISCORD_CHANNEL_NAMES = os.environ.get('DISCORD_CHANNEL_NAMES', 'minecraft, minecraft-test').replace(' ', '').split(',')
+DISCORD_GUILD_IDS = list(map(int, os.environ.get('DISCORD_GUILD_IDS', '').replace(' ', '').split(',')))
 
 # secret
 HOUR_FOR_IMAGE_LEAVE_ALONE_LONG_TIME = os.environ.get('HOUR_FOR_IMAGE_LEAVE_ALONE_LONG_TIME', None)
-ALLOW_PROCESS_KILL_COMMAND = os.environ.get('ALLOW_PROCESS_KILL_COMMAND', None)
 
 if HOUR_FOR_IMAGE_LEAVE_ALONE_LONG_TIME == None:
   HOUR_FOR_IMAGE_LEAVE_ALONE_LONG_TIME = ''
-
-if ALLOW_PROCESS_KILL_COMMAND == 'true':
-  ALLOW_PROCESS_KILL_COMMAND = True
-else:
-  ALLOW_PROCESS_KILL_COMMAND = False
